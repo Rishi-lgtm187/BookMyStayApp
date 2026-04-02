a9b58ba24199c21c5c237c92c8588c004c5fef77
Sabari-0407 <sm9120@srmist.edu.in>
2026-04-02 08:43:49 +0530

    [Rishi] BookMyStayApp.java

--- App/src/BookMyStayApp.java
+++ App/src/BookMyStayApp.java
@@ -1,5 +1,17 @@
+import java.util.HashMap;
+
 public class BookMyStayApp {
-    public static void main(String[] args){
-        System.out.println("Hotel Booking System v1.0");
+    public static void main(String[] args) {
+        HashMap<String, Integer> inventory = new HashMap<>();
+
+        inventory.put("Single Room", 5);
+        inventory.put("Double Room", 3);
+        inventory.put("Suite Room", 2);
+
+        System.out.println("--- Hotel Room Inventory ---");
+
+        for (String roomType : inventory.keySet()) {
+            System.out.println(roomType + " Available: " + inventory.get(roomType));
+        }
     }
 }
